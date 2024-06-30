b7d20918ce799d9ff1d86ddb28059c9816f9748b
Vasilii Stakrotckii <inferno681@gmail.com>
2024-06-30 10:45:58 +0000

    service ready. Storages - List

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,43 @@
-from dataclasses import dataclass
+from datetime import timedelta
+from dataclasses import dataclass, field
+from datetime import datetime
+from enum import Enum
+
+transaction_storage = []
+report_storage = []
+
+
+class TransactionType(Enum):
+    DEBIT = "списание"
+    CREDIT = "пополнение"
 
 
 @dataclass
 class Transaction:
     ID: int
     sum: int
-    type:
+    type: TransactionType
+    created_at: datetime = field(default_factory=lambda: datetime.now())
+
+
+class TransactionService():
+    @staticmethod
+    def create_transaction(ID: int, sum: int, type: TransactionType) -> Transaction:
+        transaction = Transaction(ID=ID, sum=sum, type=type)
+        transaction_storage.append(transaction)
+        return transaction
+
+    @staticmethod
+    def transactions_report(ID: int, start_date: datetime, end_date: datetime) -> dict[str, any]:
+        transactions = [
+            transaction for transaction in transaction_storage
+            if transaction.ID == ID and start_date <= transaction.created_at <= end_date
+        ]
+        report = {
+            "Transactions": transactions,
+            TransactionType.DEBIT: sum(t.sum for t in transactions if t.type == TransactionType.DEBIT),
+            TransactionType.CREDIT: sum(
+                t.sum for t in transactions if t.type == TransactionType.CREDIT)
+        }
+        report_storage.append(report)
+        return report
